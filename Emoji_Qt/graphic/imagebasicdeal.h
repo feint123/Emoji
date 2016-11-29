14971905c97f71c2a509a6d83b42b58b13bec09a
feint123 <1084645640@qq.com>
2016-11-29 20:36:12 +0800

    continue

--- Emoji_Qt/graphic/imagebasicdeal.h
+++ Emoji_Qt/graphic/imagebasicdeal.h
@@ -1,11 +1,18 @@
 #ifndef IMAGEBASICDEAL_H
 #define IMAGEBASICDEAL_H
 
+#include <QImage>
+#include <QSize>
+
+
 
 class ImageBasicDeal
 {
 public:
     ImageBasicDeal();
+    static void cutImage(QImage *image,QPoint startPoint,QSize size);
+    static void scaleImage(QImage *image,double scale,bool isZoomOut);
+
 };
 
 #endif // IMAGEBASICDEAL_H
--- Emoji_Qt/graphic/imagerender.h
+++ Emoji_Qt/graphic/imagerender.h
@@ -1,11 +1,28 @@
 #ifndef IMAGERENDER_H
 #define IMAGERENDER_H
 
+#include <QImage>
+
+
 
 class ImageRender
 {
 public:
     ImageRender();
+    //生成灰度图
+    static void grayImg(QImage *image,double gray=0.5);
+    QImage getImage() const;
+    void setImage(const QImage &value);
+    //修改图片的亮度
+    QImage changeLighting(double light);
+    //修改图片质量，最大为1.0，最小为0.0
+    QImage changeQuarty(double q=1.0);
+    //修改图片的对比度
+    QImage changeContrast();
+    //修改图片rgb的比例
+    QImage changeRgb(int r=50,int b=50,int g=50);
+private:
+    QImage image;
 };
 
 #endif // IMAGERENDER_H
